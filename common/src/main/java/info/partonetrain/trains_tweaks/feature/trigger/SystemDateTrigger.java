b29fb49f1e4feee4899d962806d9248167322d55
Partonetrain <Partonetrain@gmail.com>
2026-05-18 19:22:59 -0500

    SystemDateTrigger validation

--- common/src/main/java/info/partonetrain/trains_tweaks/feature/trigger/SystemDateTrigger.java
+++ common/src/main/java/info/partonetrain/trains_tweaks/feature/trigger/SystemDateTrigger.java
@@ -1,6 +1,7 @@
 package info.partonetrain.trains_tweaks.feature.trigger;
 
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.DataResult;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.advancements.critereon.ContextAwarePredicate;
 import net.minecraft.advancements.critereon.EntityPredicate;
@@ -9,6 +10,7 @@ import net.minecraft.advancements.critereon.SimpleCriterionTrigger;
 import net.minecraft.server.level.ServerPlayer;
 import org.jetbrains.annotations.NotNull;
 import java.time.LocalDate;
+import java.time.Year;
 import java.util.Optional;
 
 public class SystemDateTrigger extends SimpleCriterionTrigger<SystemDateTrigger.TriggerInstance> {
@@ -25,9 +27,12 @@ public class SystemDateTrigger extends SimpleCriterionTrigger<SystemDateTrigger.
         public static final Codec<SystemDateTrigger.TriggerInstance> CODEC = RecordCodecBuilder.create(
                 p_337396_ -> p_337396_.group(
                     EntityPredicate.ADVANCEMENT_CODEC.optionalFieldOf("player").forGetter(SystemDateTrigger.TriggerInstance::player),
-                    MinMaxBounds.Ints.CODEC.optionalFieldOf("year", MinMaxBounds.Ints.ANY).forGetter(SystemDateTrigger.TriggerInstance::year),
-                    MinMaxBounds.Ints.CODEC.optionalFieldOf("month", MinMaxBounds.Ints.between(1, 12)).forGetter(SystemDateTrigger.TriggerInstance::month),
-                    MinMaxBounds.Ints.CODEC.optionalFieldOf("day", MinMaxBounds.Ints.between(1, 31)).forGetter(SystemDateTrigger.TriggerInstance::day)
+                    MinMaxBounds.Ints.CODEC.optionalFieldOf("year", MinMaxBounds.Ints.ANY)
+                            .validate(SystemDateTrigger::validateYear).forGetter(SystemDateTrigger.TriggerInstance::year),
+                    MinMaxBounds.Ints.CODEC.optionalFieldOf("month", MinMaxBounds.Ints.between(1, 12))
+                            .validate(SystemDateTrigger::validateMonth).forGetter(SystemDateTrigger.TriggerInstance::month),
+                    MinMaxBounds.Ints.CODEC.optionalFieldOf("day", MinMaxBounds.Ints.between(1, 31))
+                            .validate(SystemDateTrigger::validateDay).forGetter(SystemDateTrigger.TriggerInstance::day)
             ).apply(p_337396_, SystemDateTrigger.TriggerInstance::new));
 
 
@@ -36,5 +41,28 @@ public class SystemDateTrigger extends SimpleCriterionTrigger<SystemDateTrigger.
             //if it works, it works...
             return year.matches(LocalDate.now().getYear()) && month.matches(LocalDate.now().getMonthValue()) && day.matches(LocalDate.now().getDayOfMonth());
         }
+    }
+
+    public static DataResult<MinMaxBounds.Ints> validateYear(MinMaxBounds.Ints year){
+        if (year.min().get() < Year.MIN_VALUE || year.max().get() > Year.MAX_VALUE){
+            return DataResult.error(() -> "Invalid year: " + year);
+        }
+
+        return DataResult.success(year);
+    }
+
+    public static DataResult<MinMaxBounds.Ints> validateMonth(MinMaxBounds.Ints month){
+        if (month.min().get() < 1 || month.max().get() > 12){
+            return DataResult.error(() -> "Invalid month: " + month);
+        }
+
+        return DataResult.success(month);
+    }
+
+    public static DataResult<MinMaxBounds.Ints> validateDay(MinMaxBounds.Ints day){
+        if (day.min().get() < 1 || day.max().get() > 31){
+            return DataResult.error(() -> "Invalid day: " + day);
+        }
+        return DataResult.success(day);
     }
 }
